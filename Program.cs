e7df79e188f9cad994d606dbce88425dda7f70dc
mahlarauf-commits <mahlarauf@gmail.com>
2025-12-09 16:11:21 +0330

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+{Console.WriteLine("Enter number1:");
+int number1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter number2:");
+int number2 = int.Parse(Console.ReadLine());
+
+int sum = number1 + number2;
+Console.WriteLine("Sum: " + sum);}
+
