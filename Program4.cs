f549512bf73e36caa75053d7c26cd64cf4c60c90
mahlarauf-commits <mahlarauf@gmail.com>
2025-12-10 12:06:21 +0330

    Add files via upload

---
+++ Program4.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter a number of week:");
+int dayofweek = int.Parse(Console.ReadLine());
+
+switch (dayofweek)
+{
+	case 1:
+        Console.WriteLine("Saturday");
+        break;
+    case 2:
+        Console.WriteLine("Sunday");
+        break ;
+    case 3:
+        Console.WriteLine("Monday");
+        break;
+     case 4:
+        Console.WriteLine("Tuesday");
+        break;
+     case 5:
+        Console.WriteLine("Wednesday");
+        break;
+     case 6:
+        Console.WriteLine("Thursday");
+        break;
+     case 7:
+        Console.WriteLine("Friday");
+        break;
+    default:
+        Console.WriteLine("please enter a number in the range 1 to 7");
+        break;
+}
