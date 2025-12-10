da7900714e482bbfd109d650a723a177cdee9714
mahlarauf-commits <mahlarauf@gmail.com>
2025-12-10 10:26:36 +0330

    Add files via upload

---
+++ Program2.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter a number:");
+int i = int.Parse(Console.ReadLine());
+
+if (i > 0) ;
+{
+    Console.WriteLine("mosbat");
+}
