d3c18e6625e312aee259f1894eaee9ed533d8f1d
mahlarauf-commits <mahlarauf@gmail.com>
2025-12-10 11:33:11 +0330

    Program.cs

--- Program.cs
+++ Program.cs
@@ -1,35 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Enter a number of week:");
-byte dayofweek = byte.parse(Console.ReadLine());
-if (dayofweek == 1 )
-{
-    Console.WriteLine("Saturday");
-}
-else if (dayofweek==2)
-{
-    Console.WriteLine("Sunday");
-}
-else if (dayofweek==3)
-{
-    Console.WriteLine("Monday");
-}
-else if (dayofweek==4)
-{
-    Console.WriteLine("Tuesday");
-}
-else if (dayofweek==5)
-{
-    Console.WriteLine("Wednesday");
-}
-else if (dayofweek==6)
-{
-    Console.WriteLine("Thursday");
-}
-else if (dayofweek==7)
-{
-    Console.WriteLine("Friday");
-}
-else
-{
-    Console.WriteLine("please enter a number in the range 1 to 7");
-}
+// See https://aka.ms/new-console-template for more information
+{Console.WriteLine("Enter number1:");
+int number1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter number2:");
+int number2 = int.Parse(Console.ReadLine());
+
+int sum = number1 + number2;
+Console.WriteLine("Sum: " + sum);}
