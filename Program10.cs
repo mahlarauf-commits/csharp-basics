68441cb004e680bafd58969f8663b8e4f855cb0e
mahlarauf-commits <mahlarauf@gmail.com>
2025-12-14 15:28:48 +0330

    Program10.cs

--- Program10.cs
+++ Program10.cs
@@ -1,1 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        Image file;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK) 
+            {
+                file = Image.FromFile(openFileDialog1.FileName);
+                pictureBox1.Image = file;
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialog2.ShowDialog();
+            if (result == DialogResult.OK)
+            { 
+                file.Save(openFileDialog2.FileName);
+            }
+        }
+    }
+}
