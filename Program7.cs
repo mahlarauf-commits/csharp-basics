aa7107ffcaf6635239f80af7fd605c11fb68265f
mahlarauf-commits <mahlarauf@gmail.com>
2025-12-14 10:05:51 +0330

    Program7.cs

--- Program7.cs
+++ Program7.cs
@@ -1,1 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string name;
+            string lastname;
+            name = txtname.Text;
+            lastname = txtlastname.Text;
+            label3.Text = name + "   " + lastname;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+             int n1 , n2 ;  
+            n1 = int.Parse(textBox1.Text);
+            n2= int.Parse(textBox2.Text);
+            int result = n1 + n2;
+            label6.Text = $" n1:{n1} + n2:{n2} = {result}";
+
+        }
+    }
+}
